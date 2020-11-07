ab6ef00d34246337da86256356aceb89e3848499
innervehogwarts <73355675+innervehogwarts@users.noreply.github.com>
2020-11-08 03:31:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function myFunction(){
 var txt;
-var person = prompt("Please enter your username:", "Harry Potter");
+var person = prompt("Please enter your HackerRank username:", "Harry Potter");
 var check= false;
 var x;
 for (x=0;x<11;x++)
